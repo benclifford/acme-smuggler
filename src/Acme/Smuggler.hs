c93073473fffad1cdee9386af0a2a719ef13c83e
Ben Clifford <benc@hawaga.org.uk>
2017-10-03 13:00:05 +0100

    loosen types to allow smuggling in any haskell type, notably even Void

--- src/Acme/Smuggler.hs
+++ src/Acme/Smuggler.hs
@@ -8,17 +8,17 @@ import System.IO.Unsafe (unsafePerformIO)
 import Control.Exception (try, throw, evaluate)
 
 -- | fancy name for smuggle
-inj :: Typeable t => t -> ()
+inj :: Typeable t => t -> a
 inj = smuggle
 
 -- | fancy name for discover
-prj :: Typeable t => () -> Maybe t
+prj :: Typeable t => a -> Maybe t
 prj = discover
 
-smuggle :: Typeable t => t -> ()
+smuggle :: Typeable t => t -> a
 smuggle = throw . toDyn
 
-discover :: Typeable t => () -> Maybe t
+discover :: Typeable t => a -> Maybe t
 discover = either (fromDynamic) (const Nothing)
   . unsafePerformIO
   . try
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,14 +1,27 @@
 import Acme.Smuggler
+import Data.Typeable (Typeable)
+import Data.Void (Void)
 import Test.Hspec
 
 main :: IO ()
 main = hspec $ do
-  describe "prj . inj" $ do
-    it "conveys an integer" $ (prj . inj) (7 :: Integer) `shouldBe` Just (7 :: Integer)
+  describe "prj . inj via ()" $ do
+    it "conveys an integer" $ (prj . injUnit) (7 :: Integer) `shouldBe` Just (7 :: Integer)
     it "gives Nothing when going from an integer to a string" $
-      (prj . inj) (7 :: Integer) `shouldBe` (Nothing :: Maybe String)
+      (prj . injUnit) (7 :: Integer) `shouldBe` (Nothing :: Maybe String)
 
   describe "prj" $ do
     it "gives Nothing when projecting from a real ()" $
       (prj ()) `shouldBe` (Nothing :: Maybe Integer)
 
+
+  describe "prj . inj via Void" $ do
+    it "conveys an integer" $ (prj . injVoid) (7 :: Integer) `shouldBe` Just (7 :: Integer)
+    it "gives Nothing when going from an integer to a string" $
+      (prj . injVoid) (7 :: Integer) `shouldBe` (Nothing :: Maybe String)
+
+injUnit :: Typeable t => t -> ()
+injUnit = inj
+
+injVoid :: Typeable t => t -> Void
+injVoid = inj
